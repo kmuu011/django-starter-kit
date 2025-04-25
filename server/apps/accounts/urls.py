2fd88447c9f0aedebfedb5041921070aaa8ba858
강민우 <kmuu011@naver.com>
2025-04-25 17:33:47 +0900

    요청 url 끝에 Slash가 없을경우 404가 되게 구성

--- server/apps/accounts/urls.py
+++ server/apps/accounts/urls.py
@@ -3,6 +3,6 @@ from .views import RegisterView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-  path('register', RegisterView.as_view(), name='register'),
-  path('login', obtain_auth_token, name='login'),
+  path('register/', RegisterView.as_view(), name='register'),
+  path('login/', obtain_auth_token, name='login'),
 ]
