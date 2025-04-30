3faf15aec51beaba4db1c9d1af61fa4d525163e1
강민우 <kmuu011@naver.com>
2025-04-30 13:57:45 +0900

    무중단 배포 명령 일부 수정

--- controller_for_window/release-server.bat
+++ controller_for_window/release-server.bat
@@ -2,7 +2,10 @@ cd ..
 cd docker-compose
 cd django_starter_kit_production
 
-docker compose down django_0
-docker compose up django_0 -d
-docker compose down django_1
-docker compose up django_1 -d
+docker compose stop django_0
+docker compose rm -f django_0
+docker compose up -d --no-deps django_0
+
+docker compose stop django_1
+docker compose rm -f django_1
+docker compose up -d --no-deps django_1
